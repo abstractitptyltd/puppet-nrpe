89159ac917d30fcac4d82facaeb4b49d829d270b
Pete Brown <rendhalver@gmail.com>
2013-03-04 10:26:49 +1000

    modified:   manifests/init.pp 	class chaining was not working properly 	strange behaviour which may be related to a known bug

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,22 @@
 class nrpe {
+  include nrpe::params
+  include nrpe::users
+  include nrpe::install
+  include nrpe::config
+  include nrpe::service
+  include nrpe::firewall
+  include nrpe::monitoring
+  Class[nrpe::params] ->
+  Class[nrpe::users] ->
+  Class[nrpe::install] ->
+  Class[nrpe::config] ~>
+  Class[nrpe::service] ->
+  Class[nrpe::firewall] ->
+  Class[nrpe::monitoring] ->
+  Class[nrpe]
+
+##  This method of class chaining doesn't seem to be working properly
+/*
   class{'nrpe::params':} ->
   class{'nrpe::install':} ->
   class{'nrpe::config':} ~>
@@ -6,4 +24,5 @@ class nrpe {
   class{'nrpe::firewall':} ->
   class{'nrpe::monitoring':} ->
   Class['nrpe']
+*/
 }
